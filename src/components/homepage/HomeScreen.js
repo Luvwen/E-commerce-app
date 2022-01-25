07dad619ebaf7d6defee45ace843c2718e70af54
unknown <santiagobalino@hotmail.com>
2022-01-25 03:54:30 -0300

    refactor(Root): Created carrousel component to make more legible the code.

--- src/components/homepage/HomeScreen.js
+++ src/components/homepage/HomeScreen.js
@@ -5,6 +5,7 @@ import './styles.css';
 import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa';
 
 import { devices } from '../data/devices';
+import { Carrousel } from './Carrousel';
 
 export const HomeScreen = () => {
   const [current, setCurrent] = useState(0);
@@ -33,27 +34,7 @@ export const HomeScreen = () => {
           className='carrousel__rightarrow'
           onClick={nextSlide}
         />
-        {devices.map((device, index) => (
-          <div
-            className={index === current ? 'slide-active' : 'slide'}
-            key={index}
-          >
-            {index === current && (
-              <div className='slide-images'>
-                <img
-                  src={device.img}
-                  alt={device.model}
-                  className='carrousel__image'
-                ></img>
-                <div className='slider'>
-                  <p className='slider__info'>Fabricante: {device.brand}</p>
-                  <p className='slider__info'>Modelo: {device.model}</p>
-                  <p className='slider__info'>Precio: {device.price}</p>
-                </div>
-              </div>
-            )}
-          </div>
-        ))}
+        <Carrousel devices={devices} current={current} />
       </div>
     </div>
   );
