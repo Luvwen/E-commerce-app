99617ac2c621b341d872fa7f21511f5cb8ca3c62
Luvwen <santiagobalino@hotmail.com>
2022-01-30 03:36:49 -0300

    refactor(Root): Refactor
    Move logic from homescreen to the carrousel component.

--- src/components/homepage/Carrousel.js
+++ src/components/homepage/Carrousel.js
@@ -1,29 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa';
+import { devices } from '../data/devices';
 
-export const Carrousel = ({ current, devices }) => {
+export const Carrousel = () => {
+  const [current, setCurrent] = useState(0);
+
+  const devicesAmount = devices.length;
+
+  const nextSlide = () => {
+    setCurrent(current === devicesAmount - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? devicesAmount - 1 : current - 1);
+  };
+
   return (
-    <div>
-      {devices.map((device, index) => (
-        <div
-          className={index === current ? 'slide-active' : 'slide'}
-          key={index}
-        >
-          {index === current && (
-            <div className='slide-images'>
-              <img
-                src={device.img}
-                alt={device.model}
-                className='carrousel__image'
-              ></img>
-              <div className='slider'>
-                <p className='slider__info'>Fabricante: {device.brand}</p>
-                <p className='slider__info'>Modelo: {device.model}</p>
-                <p className='slider__info'>Precio: {device.price}</p>
+    <>
+      <FaArrowCircleLeft className='carrousel__leftarrow' onClick={prevSlide} />
+      <FaArrowCircleRight
+        className='carrousel__rightarrow'
+        onClick={nextSlide}
+      />
+      <div>
+        {devices.map((device, index) => (
+          <div
+            className={index === current ? 'slide-active' : 'slide'}
+            key={index}
+          >
+            {index === current && (
+              <div className='slide-images'>
+                <img
+                  src={device.img}
+                  alt={device.model}
+                  className='carrousel__image'
+                ></img>
+                <div className='slider'>
+                  <p className='slider__info'>Fabricante: {device.brand}</p>
+                  <p className='slider__info'>Modelo: {device.model}</p>
+                  <p className='slider__info'>Precio: {device.price}</p>
+                </div>
               </div>
-            </div>
-          )}
-        </div>
-      ))}
-    </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
--- src/components/homepage/HomeScreen.js
+++ src/components/homepage/HomeScreen.js
@@ -1,39 +1,13 @@
-import React, { useState } from 'react';
-
+import React from 'react';
+import { Carrousel } from './Carrousel';
 import './styles.css';
 
-import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa';
-
-import { devices } from '../data/devices';
-import { Carrousel } from './Carrousel';
-
 export const HomeScreen = () => {
-  // Carrousel logic
-  const [current, setCurrent] = useState(0);
-
-  const length = devices.length;
-
-  const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
-  };
-
-  const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
-  };
-
   return (
     <div className='home'>
       <h1 className='home__title'>El futuro esta al alcance de tu mano</h1>
       <div className='carrousel'>
-        <FaArrowCircleLeft
-          className='carrousel__leftarrow'
-          onClick={prevSlide}
-        />
-        <FaArrowCircleRight
-          className='carrousel__rightarrow'
-          onClick={nextSlide}
-        />
-        <Carrousel devices={devices} current={current} />
+        <Carrousel />
       </div>
     </div>
   );
